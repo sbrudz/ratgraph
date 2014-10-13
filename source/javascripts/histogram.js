f71140192815a8ef3f2c2606dc5976020ebe4e86
sbrudz <sbrudz@gmail.com>
2014-10-13 00:53:30 -0400

    Added basic histogram to main page

--- source/javascripts/histogram.js
+++ source/javascripts/histogram.js
@@ -1,8 +1,10 @@
-
-// Thanks to http://stackoverflow.com/questions/17745682/d3-histogram-date-based
-
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 1200 - margin.left - margin.right,
+var margin = {
+        top: 10,
+        right: 30,
+        bottom: 30,
+        left: 50
+    },
+    width = 760 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 var parseDate = d3.time.format("%m/%d/%Y %I:%M:%S %p").parse;
@@ -13,74 +15,90 @@ var x = d3.time.scale().range([0, width]);
 var y = d3.scale.linear().range([height, 0]);
 
 var xAxis = d3.svg.axis().scale(x)
-  .orient("bottom").tickFormat(formatDate);
+    .orient("bottom").tickFormat(formatDate);
 
 var yAxis = d3.svg.axis().scale(y)
-  .orient("left").ticks(6);
+    .orient("left").ticks(6);
 
-var svg = d3.select("div#d3_histogram")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// Create the SVG drawing area
+var svg = d3.select("#basic-histogram")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Get the data
 d3.csv("data/Rat_Sightings.csv", function(error, data) {
-  data.forEach(function(d) {
-    d.created_date = parseDate(d["Created Date"]);
-  });
 
-  var monthExtent = d3.extent(data, function(d) { return d.created_date; });
-  var monthBins = d3.time.months(d3.time.month.offset(monthExtent[0],-1),
-                                 d3.time.month.offset(monthExtent[1],1));
+    // Parse the date strings into javascript dates
+    data.forEach(function(d) {
+        d.created_date = parseDate(d["Created Date"]);
+    });
 
-  var histData = d3.layout.histogram()
-    .value(function(d) { return d.created_date; })
-    .bins(monthBins)
-    (data);
+    // Determine the first and list dates in the data set
+    var monthExtent = d3.extent(data, function(d) {
+        return d.created_date;
+    });
 
-  // Scale the range of the data
-  x.domain(d3.extent(monthBins));
-  y.domain([0, d3.max(histData, function(d) { return d.y; })]);
+    // Create one bin per month, use an offset to include the first and last months
+    var monthBins = d3.time.months(d3.time.month.offset(monthExtent[0], -1),
+        d3.time.month.offset(monthExtent[1], 1));
 
-  var bar = svg.selectAll(".bar")
-      .data(histData)
-   .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) { return "translate(" + (x(d.x)) + "," + y(d.y) + ")"; });
+    // Use the histogram layout to create a function that will bin the data
+    var binByMonth = d3.layout.histogram()
+        .value(function(d) {
+            return d.created_date;
+        })
+        .bins(monthBins);
 
-  bar.append("rect")
-      .attr("x", 1)
-      .attr("width", x(new Date(histData[0].x.getTime() + histData[0].dx))-5)
-      .attr("height", function(d) { return height - y(d.y); });
+    // Bin the data by month
+    var histData = binByMonth(data);
 
-  // Add the X Axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    // Scale the range of the data by setting the domain
+    x.domain(d3.extent(monthBins));
+    y.domain([0, d3.max(histData, function(d) {
+        return d.y;
+    })]);
 
-  // Add the Y Axis
-  svg.append("g")
-     .attr("class", "y axis")
-     .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Number of Sightings");
+    // Set up one bar for each bin
+    // Months have slightly different lengths so calculate the width for each bin
+    // Note: dx, the width of the histogram bin, is in milliseconds so convert the x value
+    // into UTC time and convert the sum back to a date in order to help calculate the width
+    // Thanks to npdoty for pointing this out in this stack overflow post:
+    // http://stackoverflow.com/questions/17745682/d3-histogram-date-based
+    svg.selectAll(".bar")
+        .data(histData)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) {
+            return x(d.x);
+        })
+        .attr("width", function(d) {
+            return x(new Date(d.x.getTime() + d.dx)) - x(d.x) - 1;
+        })
+        .attr("y", function(d) {
+            return y(d.y);
+        })
+        .attr("height", function(d) {
+            return height - y(d.y);
+        });
 
-  var graph = new Rickshaw.Graph({
-        element: document.querySelector("#rickshaw_histogram"),
-        renderer: 'bar',
-        series: [{
-                data: histData.map(function(d) { var pair = {}; pair["x"] = +d.x; pair["y"] = d.y; return pair; }),
-                color: 'steelblue'
-        }]
-  });
-  var axes = new Rickshaw.Graph.Axis.Time( { graph: graph } );
-  graph.render();
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis and label
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Number of Sightings");
 
 });
