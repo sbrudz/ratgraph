2ed9cdcdc7ee352ca612bdf0594ec4efdd0d4d97
sbrudz <sbrudz@gmail.com>
2014-10-23 14:47:34 -0400

    Switched to using the queue library for loading files. Added demographics csv

--- source/javascripts/ratviz.js
+++ source/javascripts/ratviz.js
@@ -1,15 +1,25 @@
-var margin = {top: 10, left: 10, bottom: 10, right: 10};
+"use strict";
 
-function calculateSvgSize(id, margin, heightRatio) {
-	var width = parseInt(d3.select(id).style('width'))
-	  , height = width * heightRatio
-	  , width = Math.max(100,width - margin.left - margin.right)
-	  , height = Math.max(150,height - margin.top - margin.bottom);
-	return {height: height, width: width};  
+var margin = {
+    top: 10,
+    left: 10,
+    bottom: 10,
+    right: 10
 };
 
+function calculateSvgSize(id, margin, heightRatio) {
+    var width = parseInt(d3.select(id).style('width')),
+        height = width * heightRatio;
+    width = Math.max(100, width - margin.left - margin.right);
+    height = Math.max(150, height - margin.top - margin.bottom);
+    return {
+        height: height,
+        width: width
+    };
+}
+
 var mapSize = calculateSvgSize('#choropleth', margin, 1.2);
-var histSize = calculateSvgSize('#histogram', margin, .33);
+var histSize = calculateSvgSize('#histogram', margin, 0.33);
 var boroughSize = calculateSvgSize('#boroughRow', margin, 1.2);
 var typeSize = calculateSvgSize('#typeRow', margin, 1.2);
 var topZipCodesSize = calculateSvgSize('#topZipCodes', margin, 1.2);
@@ -20,7 +30,7 @@ var formatCount = d3.format(",.0f");
 
 
 // Thanks to http://colorbrewer2.org/
-var colorScale = d3.scale.quantize().range(['rgb(255,255,229)','rgb(247,252,185)','rgb(217,240,163)','rgb(173,221,142)','rgb(120,198,121)','rgb(65,171,93)','rgb(35,132,67)','rgb(0,104,55)','rgb(0,69,41)']);
+var colorScale = d3.scale.quantize().range(['rgb(255,255,229)', 'rgb(247,252,185)', 'rgb(217,240,163)', 'rgb(173,221,142)', 'rgb(120,198,121)', 'rgb(65,171,93)', 'rgb(35,132,67)', 'rgb(0,104,55)', 'rgb(0,69,41)']);
 
 // Figure out scale and translate for geo projection
 // Thanks to http://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
@@ -39,213 +49,225 @@ var boroughRow = dc.rowChart("#boroughRow");
 var typeRow = dc.rowChart("#typeRow");
 var topZipCodes = dc.rowChart("#topZipCodes");
 
-d3.csv("data/nyc_rodent_complaints_cleaned.csv", function(error, rawData) {
+queue()
+    .defer(d3.csv, "data/nyc_rodent_complaints_cleaned.csv")
+    .defer(d3.json, "data/nyc-zip-code.json")
+    .await(ready);
 
-	rawData.forEach(function(d) {
-		d.created_date = new Date(+d["Created Date"]);
-	});
+function ready(error, rawData, nycZipJson) {
 
-	var data = crossfilter(rawData);
-	var all = data.groupAll();
+    if (error) {
+        console.warn(error);
+    }
 
-	var zipCodes = data.dimension(function(d) {
-		return d["Incident Zip"];
-	});
-	var zipCodeCounts = zipCodes.group();
+    rawData.forEach(function (d) {
+        d.created_date = new Date(+d["Created Date"]);
+    });
 
-	var time = data.dimension(function(d) {
-	    return d.created_date;
-	});
-	var timeCounts = time.group(function(d) {
-		return d3.time.month(d);
-	}).reduceCount();
+    var data = crossfilter(rawData),
+    	all = data.groupAll();
 
-	var borough = data.dimension(function(d) {
-		return d["Borough"];
-	});
-	var boroughCounts = borough.group().reduceCount();
+    var zipCodes = data.dimension(function (d) {
+        return d["Incident Zip"];
+    });
+    var zipCodeCounts = zipCodes.group();
 
-	var type = data.dimension(function(d) {
-		return d["Descriptor"];
-	});
-	var typeCounts = type.group().reduceCount();
+    var time = data.dimension(function (d) {
+        return d.created_date;
+    });
+    var timeCounts = time.group(function (d) {
+        return d3.time.month(d);
+    }).reduceCount();
 
-	// Determine the first and last dates in the data set
-	var monthExtent = d3.extent(rawData, function(d) { return d.created_date; });
+    var borough = data.dimension(function (d) {
+        return d["Borough"];
+    });
+    var boroughCounts = borough.group().reduceCount();
 
+    var type = data.dimension(function (d) {
+        return d["Descriptor"];
+    });
+    var typeCounts = type.group().reduceCount();
 
-	var timeScale = d3.time.scale()
-		.domain([monthExtent[0], monthExtent[1]])
-		.nice(d3.time.month);
-
-	var shortMonthTickFormat = d3.time.format.multi([
-			["%b", function(d) { return d.getMonth(); }],
-			["%Y", function() { return true; }]
-	]);
-
-	d3.json("data/nyc-zip-code.json", function(nycZipJson) {
-
-		// Compute the bounds of a feature of interest, then derive scale & translate.
-		var b = path.bounds(nycZipJson),
-		    s = .95 / Math.max((b[1][0] - b[0][0]) / mapSize.width, (b[1][1] - b[0][1]) / mapSize.height),
-		    t = [(mapSize.width - s * (b[1][0] + b[0][0])) / 2, (mapSize.height - s * (b[1][1] + b[0][1])) / 2];
-
-		// Update the projection to use computed scale & translate.
-		projection
-		    .scale(s)
-		    .translate(t);
-
-		choropleth.width(mapSize.width)
-			.height(mapSize.height)
-			.dimension(zipCodes)
-			.group(zipCodeCounts, "Rat Sightings by Zip Code")
-			.colors(colorScale)
-			.colorAccessor(function(d) {
-				return d.value;
-			})
-			.colorCalculator(function(d) {
-				return d ? choropleth.colors()(d) : 'lightgray';
-			})
-			.overlayGeoJson(nycZipJson.features, "zip_code", function(d) {
-				return d.properties.ZIP;
-			})
-			.projection(projection)
-			.title(function(d) {
-				return "Zip Code: " + d.key + "\nNumber of Sightings: " + d.value;
-			});
-
-		choropleth.calculateColorDomain();
-
-		// Create a legend for the choropleth
-		// Thanks to http://eyeseast.github.io/visible-data/2013/08/27/responsive-legends-with-d3/
-		var legend = d3.select('#legend')
-			.append('ul')
-		    .attr('class', 'list-inline');
-
-		var keys = legend.selectAll('li.key')
-		    .data(colorScale.range());
-
-		keys.enter().append('li')
-		    .attr('class', 'key')
-		    .style('border-left-color', String)
-		    .text(function(d) {
-		        var r = colorScale.invertExtent(d);
-		        return formatCount(r[0]);
-		    });
-
-		histogram.width(histSize.width)
-			.height(histSize.height)
-			.margins({top: 10, right: 10, bottom: 20, left: 40})
-			.dimension(time)
-			.group(timeCounts)
-			.x(timeScale)
-			.round(d3.time.month.round)
-			.xUnits(d3.time.months)
-			.elasticY(true)
-			.renderHorizontalGridLines(true);
-
-		histogram.xAxis().tickFormat(shortMonthTickFormat);
-
-        boroughRow
-        	.width(boroughSize.width)
-        	.height(boroughSize.height)
-        	.dimension(borough)
-        	.group(boroughCounts)
-        	.elasticX(true)
-        	.ordering(function (d) {
-        		return -d.value;
-        	})
-        	;
-
-		boroughRow.xAxis().ticks(5);
-		boroughRow.margins().right = 5;
-		boroughRow.margins().left = 5;
-
-		typeRow
-			.width(typeSize.width)
-			.height(typeSize.height)
-			.dimension(type)
-			.group(typeCounts)
-			.elasticX(true)
-        	.ordering(function (d) {
-        		return -d.value;
-        	})
-        	;
-
-		typeRow.xAxis().ticks(5);
-		typeRow.margins().right = 5;
-		typeRow.margins().left = 5;
-
-		topZipCodes
-			.width(topZipCodesSize.width)
-			.height(topZipCodesSize.height)
-			.dimension(zipCodes)
-			.group(zipCodeCounts)
-			.colors(colorScale)
-			.colorAccessor(function(d) {
-				return d.value;
-			})
-			.data(function(group) {
-				return group.top(5);
-			})
-			.elasticX(true)
-        	.ordering(function (d) {
-        		return -d.value;
-        	})
-        	;
-
-		topZipCodes.xAxis().ticks(5);
-		topZipCodes.margins().right = 5;
-		topZipCodes.margins().left = 5;
+    // Determine the first and last dates in the data set
+    var monthExtent = d3.extent(rawData, function (d) {
+        return d.created_date;
+    });
 
 
-		var updateChloroplethScale = function(chart, filter) {
-			var domain = [d3.min(choropleth.group().all(), choropleth.colorAccessor()),
-						  d3.max(choropleth.group().all(), choropleth.colorAccessor())];
-			choropleth.colorDomain(domain);
-		}
-		boroughRow.on('filtered', updateChloroplethScale);
-		typeRow.on('filtered', updateChloroplethScale);
-		histogram.on('filtered', updateChloroplethScale);
+    var timeScale = d3.time.scale()
+        .domain([monthExtent[0], monthExtent[1]])
+        .nice(d3.time.month);
 
-		choropleth.renderlet(function(chart) {
-			d3.select('#legend').selectAll('li.key')
-			    .data(chart.colors().range())
-			    .text(function(d) {
-			        var r = chart.colors().invertExtent(d);
-			        return formatCount(r[0]);
-			    });
+    var shortMonthTickFormat = d3.time.format.multi([
+        ["%b", function (d) {
+            return d.getMonth();
+        }],
+        ["%Y", function () {
+            return true;
+        }]
+    ]);
 
-		});
+    // Compute the bounds of a feature of interest, then derive scale & translate.
+    var b = path.bounds(nycZipJson),
+        s = 0.95 / Math.max((b[1][0] - b[0][0]) / mapSize.width, (b[1][1] - b[0][1]) / mapSize.height),
+        t = [(mapSize.width - s * (b[1][0] + b[0][0])) / 2, (mapSize.height - s * (b[1][1] + b[0][1])) / 2];
 
-		resize();
-	});
+    // Update the projection to use computed scale & translate.
+    projection
+        .scale(s)
+        .translate(t);
 
-});
+    choropleth.width(mapSize.width)
+        .height(mapSize.height)
+        .dimension(zipCodes)
+        .group(zipCodeCounts, "Rat Sightings by Zip Code")
+        .colors(colorScale)
+        .colorAccessor(function (d) {
+            return d.value;
+        })
+        .colorCalculator(function (d) {
+            return d ? choropleth.colors()(d) : 'lightgray';
+        })
+        .overlayGeoJson(nycZipJson.features, "zip_code", function (d) {
+            return d.properties.ZIP;
+        })
+        .projection(projection)
+        .title(function (d) {
+            return "Zip Code: " + d.key + "\nNumber of Sightings: " + d.value;
+        });
+
+    choropleth.calculateColorDomain();
+
+    // Create a legend for the choropleth
+    // Thanks to http://eyeseast.github.io/visible-data/2013/08/27/responsive-legends-with-d3/
+    var legend = d3.select('#legend')
+        .append('ul')
+        .attr('class', 'list-inline');
+
+    var keys = legend.selectAll('li.key')
+        .data(colorScale.range());
+
+    keys.enter().append('li')
+        .attr('class', 'key')
+        .style('border-left-color', String)
+        .text(function (d) {
+            var r = colorScale.invertExtent(d);
+            return formatCount(r[0]);
+        });
+
+    histogram.width(histSize.width)
+        .height(histSize.height)
+        .margins({
+            top: 10,
+            right: 10,
+            bottom: 20,
+            left: 40
+        })
+        .dimension(time)
+        .group(timeCounts)
+        .x(timeScale)
+        .round(d3.time.month.round)
+        .xUnits(d3.time.months)
+        .elasticY(true)
+        .renderHorizontalGridLines(true);
+
+    histogram.xAxis().tickFormat(shortMonthTickFormat);
+
+    boroughRow
+        .width(boroughSize.width)
+        .height(boroughSize.height)
+        .dimension(borough)
+        .group(boroughCounts)
+        .elasticX(true)
+        .ordering(function (d) {
+            return -d.value;
+        });
+
+    boroughRow.xAxis().ticks(5);
+    boroughRow.margins().right = 5;
+    boroughRow.margins().left = 5;
+
+    typeRow
+        .width(typeSize.width)
+        .height(typeSize.height)
+        .dimension(type)
+        .group(typeCounts)
+        .elasticX(true)
+        .ordering(function (d) {
+            return -d.value;
+        });
+
+    typeRow.xAxis().ticks(5);
+    typeRow.margins().right = 5;
+    typeRow.margins().left = 5;
+
+    topZipCodes
+        .width(topZipCodesSize.width)
+        .height(topZipCodesSize.height)
+        .dimension(zipCodes)
+        .group(zipCodeCounts)
+        .colors(colorScale)
+        .colorAccessor(function (d) {
+            return d.value;
+        })
+        .data(function (group) {
+            return group.top(5);
+        })
+        .elasticX(true)
+        .ordering(function (d) {
+            return -d.value;
+        });
+
+    topZipCodes.xAxis().ticks(5);
+    topZipCodes.margins().right = 5;
+    topZipCodes.margins().left = 5;
+
+    var updateChloroplethScale = function (chart, filter) {
+        var domain = [d3.min(choropleth.group().all(), choropleth.colorAccessor()),
+            d3.max(choropleth.group().all(), choropleth.colorAccessor())];
+        choropleth.colorDomain(domain);
+    };
+
+    boroughRow.on('filtered', updateChloroplethScale);
+    typeRow.on('filtered', updateChloroplethScale);
+    histogram.on('filtered', updateChloroplethScale);
+
+    choropleth.renderlet(function (chart) {
+        d3.select('#legend').selectAll('li.key')
+            .data(chart.colors().range())
+            .text(function (d) {
+                var r = chart.colors().invertExtent(d);
+                return formatCount(r[0]);
+            });
+
+    });
+
+    resize();
+}
 
 function resize() {
-	var mapSize = calculateSvgSize('#choropleth', margin, 1.2);
-	choropleth.height(mapSize.height).width(mapSize.width);
+    var mapSize = calculateSvgSize('#choropleth', margin, 1.2);
+    choropleth.height(mapSize.height).width(mapSize.width);
 
-	var histSize = calculateSvgSize('#histogram', margin, .33);
-	histogram.height(histSize.height).width(histSize.width);
-	histogram.xAxis().ticks(Math.max(histSize.width/50, 2));
-	histogram.yAxis().ticks(Math.max(histSize.height/50, 2));
+    var histSize = calculateSvgSize('#histogram', margin, 0.33);
+    histogram.height(histSize.height).width(histSize.width);
+    histogram.xAxis().ticks(Math.max(histSize.width / 50, 2));
+    histogram.yAxis().ticks(Math.max(histSize.height / 50, 2));
 
-	var boroughSize = calculateSvgSize('#boroughRow', margin, 1.2);
-	boroughRow.height(boroughSize.height).width(boroughSize.width);
-	boroughRow.xAxis().ticks(Math.max(boroughSize.width/50, 2));
+    var boroughSize = calculateSvgSize('#boroughRow', margin, 1.2);
+    boroughRow.height(boroughSize.height).width(boroughSize.width);
+    boroughRow.xAxis().ticks(Math.max(boroughSize.width / 50, 2));
 
-	var typeSize = calculateSvgSize('#typeRow', margin, 1.2);
-	typeRow.height(typeSize.height).width(typeSize.width);
-	typeRow.xAxis().ticks(Math.max(typeSize.width/50, 2));
+    var typeSize = calculateSvgSize('#typeRow', margin, 1.2);
+    typeRow.height(typeSize.height).width(typeSize.width);
+    typeRow.xAxis().ticks(Math.max(typeSize.width / 50, 2));
 
-	var topZipCodesSize = calculateSvgSize('#topZipCodes', margin, 1.2);
-	topZipCodes.height(topZipCodesSize.height).width(topZipCodesSize.width);
-	topZipCodes.xAxis().ticks(Math.max(topZipCodesSize.width/50, 2));
+    var topZipCodesSize = calculateSvgSize('#topZipCodes', margin, 1.2);
+    topZipCodes.height(topZipCodesSize.height).width(topZipCodesSize.width);
+    topZipCodes.xAxis().ticks(Math.max(topZipCodesSize.width / 50, 2));
 
-	dc.renderAll();
+    dc.renderAll();
 }
 
 d3.select(window).on('resize', resize);
-
